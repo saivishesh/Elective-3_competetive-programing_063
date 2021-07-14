5404b2d8e2379861b3da152e56266d67eba3540b
saivishesh <saibissu9032@msitprogram.net>
2021-07-14 14:29:47 +0530

    vishesh

--- 06-eggcartons-Python/eggcartons.py
+++ 06-eggcartons-Python/eggcartons.py
@@ -6,4 +6,13 @@
 
 def fun_eggcartons(eggs):
 	# your code goes here
-	return 1
+ a=12
+ if(eggs==0):
+     return 0
+ elif(1<eggs<=a):
+     return 1
+ elif(12<eggs<=a*2):
+     return 2
+ elif(24<eggs<=a*3):
+     return 3
+ print(fun_eggcartons)
