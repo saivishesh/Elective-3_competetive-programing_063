105a7407ba1edb7b155b730ef3a4ee1c1ca64c58
saivishesh <saibissu9032@msitprogram.net>
2021-07-28 10:53:13 +0530

    vishesh

--- 01-find_int_roots-Python/find_int_roots.py
+++ 01-find_int_roots-Python/find_int_roots.py
@@ -6,8 +6,11 @@
 # in increasing order. How does a function return multiple values? Like so:
 # return root1, root2
 
-import math
+import cmath
 def fun_find_int_roots(a, b, c):
-	return 0, 0
+    d=(b**2)-(4*a*c)
+    soln1=(-b-(cmath.sqrt(d)))/2*a
+    soln2=(-b+(cmath.sqrt(d)))/2*a
+    return soln1,soln2
 
 
